27cf87f08eabd9dc733c29cd58e9f6b01c6316e4
narendra098 <bhavanamnarendra4@gmail.com>
2023-03-29 01:29:08 +0530

    Updates

--- amazon-clone/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-baafc/us-central1/api",
-  'Access-Control-Allow-Origin': true
-});
-
-export default instance;
